2b287d8076e9dd67ce189c3ee1f5163355a93460
acid <1261535644@qq.com>
2024-06-28 07:33:44 +0800

    first commit

---
+++ src/components/MyChild.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  title?: String
+  msg?: String
+  count: Number
+}>()
+const inputVal = defineModel('inputVal')
+</script>
+
+<template>
+  <div>
+    Child
+    {{ props.title }}
+    {{ props.count }}
+    <div><input v-model="inputVal" /></div>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { computed, reactive, ref } from 'vue'
+import MyChild from '../components/MyChild.vue'
+const count = ref(0)
+const state = reactive({
+  count: 0
+})
+const author = reactive({
+  name: 'John Doe',
+  books: ['Vue 2 - Advanced Guide', 'Vue 3 - Basic Guide', 'Vue 4 - The Mystery']
+})
+const publishedBooksMessage = computed(() => {
+  return author.books.length > 0 ? 'yes' : 'no'
+})
+const increment = () => {
+  count.value++
+  author.books.push('New Book')
+}
+const stateIncrement = () => {
+  state.count++
+  author.books = []
+}
+const msg = ref('hhh')
+</script>
+
+<template>
+  <main>
+    count ----- {{ count }} state.count ----- {{ state.count }}
+    <button @click="increment">count</button>
+    <button @click="stateIncrement">state</button>
+    <p>Has published books:</p>
+    <span>{{ publishedBooksMessage }}</span>
+    <button @click="stateIncrement">state</button>
+    --------------
+    {{ msg }}
+    <MyChild v-model:inputVal="msg" :count="12" title="Mychild" msg="444" />
+  </main>
+</template>
